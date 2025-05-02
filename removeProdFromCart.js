b05d25dafdb34c7e0af9185227656d5672d020de
unknown <muktinathrajbanshi786@gmail.com>
2025-05-02 16:05:23 +0545

    incre/decre is done for cart value along with updated price

--- removeProdFromCart.js
+++ removeProdFromCart.js
@@ -1,4 +1,5 @@
 import { getCartProductFromLS } from "./getCartProducts";
+import { updateCartValue } from "./updateCartValue";
 
 export const removeProdFromCart = (id) => {
     let cartProducts = getCartProductFromLS();
--- showAddToCartCards.js
+++ showAddToCartCards.js
@@ -1,6 +1,7 @@
 import products from "./api/products.json";
 import { fetchQuantityFromCartLS } from "./fetchQuantityFromCartLS";
 import { getCartProductFromLS } from "./getCartProducts";
+import { incrementDecrement } from "./incrementDecrement";
 import { removeProdFromCart } from "./removeProdFromCart";
 
 let cartProducts = getCartProductFromLS();
@@ -33,6 +34,10 @@ const showCartProduct = () => {
 
         productClone.querySelector(".productQuantity").textContent = lSActualData.quantity;
         productClone.querySelector(".productPrice").textContent = lSActualData.price;
+
+        productClone.querySelector(".stockElement").addEventListener("click", (event) => {
+          incrementDecrement(event, id, stock, price);
+        });
 
         productClone.querySelector(".remove-to-cart-button").addEventListener("click", () => removeProdFromCart(id));
 
