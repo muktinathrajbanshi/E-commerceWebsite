ef89d1bfef9f4f2555a00a3cef6cc4c6b01f9e63
unknown <muktinathrajbanshi786@gmail.com>
2025-05-03 03:34:46 +0545

    about, product & contact section is completed along with responsive one

--- showAddToCartCards.js
+++ showAddToCartCards.js
@@ -30,6 +30,7 @@ const showCartProduct = () => {
 
         productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
         productClone.querySelector(".category").textContent = category;
+        productClone.querySelector(".productName").textContent =name;
         productClone.querySelector(".productImage").src = image;
 
 
